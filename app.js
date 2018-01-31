3bad55dc33b844fed460703f41ad3ab119dd791c
Harry Markley <tremarkley@gmail.com>
2018-01-31 11:24:00 -0800

    Refactor GroceryList and GroceryListItem to be React components

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-var GroceryList = function(props) {
-  const groceries = props.items;
-  const groceryListItems = groceries.map((grocery) =>
-    <GroceryListItem item = {grocery} />
-  );
-  return( 
-    <ul>
-      {props.items.map((groceryItem) => <GroceryListItem item = {groceryItem} />)}
-    </ul>
-  );
-};
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-var GroceryListItem = (props) => (
-  <li>{props.item}</li>
-);
+  render() {
+    return (
+      <li>{this.props.item}</li>
+    );
+  }
+}
+
+class GroceryList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.props.items.map((groceryItem) => <GroceryListItem item = {groceryItem} />)}
+      </ul>
+    );
+  }
+} 
 
 const groceries = ['toilet paper', 'bananas', 'chicken', 'eggs', 'pepper'];
 ReactDOM.render(<GroceryList items = {groceries}/>, document.getElementById('app'));
