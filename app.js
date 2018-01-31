980e76fd6fbcf820963c9df3b17f87e8571f320b
Harry Markley <tremarkley@gmail.com>
2018-01-31 11:05:12 -0800

    Create reusable GroceryListItem that dynamically renders given grocery item. Create GroceryList that dynamically renders groceryListItems into GroceryList

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// TODO
+var GroceryList = function(props) {
+  const groceries = props.items;
+  const groceryListItems = groceries.map((grocery) =>
+    <GroceryListItem item = {grocery} />
+  );
+  return( 
+    <ul>
+      {props.items.map((groceryItem) => <GroceryListItem item = {groceryItem} />)}
+    </ul>
+  );
+};
+
+var GroceryListItem = (props) => (
+  <li>{props.item}</li>
+);
+
+const groceries = ['toilet paper', 'bananas', 'chicken', 'eggs', 'pepper'];
+ReactDOM.render(<GroceryList items = {groceries}/>, document.getElementById('app'));
